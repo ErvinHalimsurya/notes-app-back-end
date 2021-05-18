c01fef065709b66d92dbb0add2c9da56085aa363
ErvinHalimsurya <halimsurya.ervin@gmail.com>
2021-05-18 15:30:28 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+const {
+    addNoteHandler,
+    getNoteHandler,
+    getNoteByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+// Menambah catatan
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+// Menampilkan daftar catatan
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getNoteHandler,
+    },
+// Menampilkan isi catatan
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+// Mengubah catatan
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+// Menghapus catatan
+    {
+        methos: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
+];
+
+module.exports = routes;
